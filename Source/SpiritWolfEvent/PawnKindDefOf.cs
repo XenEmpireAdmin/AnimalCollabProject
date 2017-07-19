18c6e610a5a7b30ea746971bf7be3a13842a9580
XenEmpireAdmin <cloudstryfe@hotmail.com>
2017-07-18 23:41:41 -0700

    Spirit wolf event

---
+++ Source/SpiritWolfEvent/PawnKindDefOf.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace SpiritWolfEvent
+{
+    [DefOf]
+    public static class PawnKindDefOf
+    {
+        public static PawnKindDef ACPSpiritwolf;
+    }
+}
